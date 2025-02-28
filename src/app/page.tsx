d08e692b051737e725cfb48acf663a8b2964b07a
Erik Perez <xerikperez@gmail.com>
2025-02-27 21:04:18 -0300

    adjusted script ceation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 // Import React and useEffect hook
 import React, { useEffect, useRef } from "react";
-import Birds from "../../public/assets/birds.js";
-import Script from "next/script";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Row, Col, Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Image from "next/image";
 import profilepic from "../../public/assets/Profile Pic.png";
 import "./page.module.css";
+import Script from "next/script";
+
 
 const VantaBackground: React.FC = () => {
-  const backgroundRef = useRef<HTMLDivElement>(null);
+  const backgroundRef = useRef<HTMLDivElement | null>(null);
+
   const vantaEffect = useRef<any>(null);
 
   useEffect(() => {
@@ -71,12 +72,12 @@ const Page: React.FC = () => {
     <div
       style={{ position: "relative", minHeight: "100vh", maxHeight: "100vh" }}
     >
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
-      <script
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js" strategy="beforeInteractive"></Script>
+      <Script
         src="
       https://cdn.jsdelivr.net/npm/vanta@0.5.24/dist/vanta.birds.min.js
-      "
-      ></script>
+      " strategy="beforeInteractive"
+      ></Script>
       <VantaBackground />
       <Navbar className="custom-navbar">
         <Container style={{ maxWidth: "1200px" }}>
@@ -137,10 +138,10 @@ const Page: React.FC = () => {
               </h3>
             </Container>
             <Container className="d-flex flex-column gap-5 mb-3 fs-5" style={{ maxWidth: "200px" }}>
-              <Button as="a" href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <Button as="a" href="https://github.com/xerikperez" target="_blank" rel="noopener noreferrer">
                 GitHub
               </Button>
-              <Button as="a" href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+              <Button as="a" href="https://linkedin.com/in/erikpérez" target="_blank" rel="noopener noreferrer">
                 LinkedIn
               </Button>
               <Button as="a" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
