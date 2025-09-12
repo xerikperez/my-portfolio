663f24a9195e6567174be52e0d614c218e0f246a
Erik Perez <89523432+xerikperez@users.noreply.github.com>
2025-09-12 12:27:30 -0400

    feat: animate circuit background

--- components/CircuitBackground.tsx
+++ components/CircuitBackground.tsx
@@ -1,6 +1,15 @@
 'use client';
+import { useEffect, useState } from 'react';
 
 export default function CircuitBackground() {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setOffset(window.scrollY * 0.2);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div
       className="fixed inset-0 -z-20 opacity-20"
@@ -8,6 +17,7 @@ export default function CircuitBackground() {
         backgroundImage: "url('/circuit-board.svg')",
         backgroundRepeat: 'repeat',
         backgroundSize: '400px 400px',
+        backgroundPosition: `0px ${-offset}px`,
       }}
     />
   );
