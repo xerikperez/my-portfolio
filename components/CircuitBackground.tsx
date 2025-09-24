f0dc2eb7626b259491290649d207d0d604b9bafb
xerikperez <xerikperez@gmail.com>
2025-09-24 16:09:03 -0400

    opacity fix

--- components/CircuitBackground.tsx
+++ components/CircuitBackground.tsx
@@ -1,22 +1,22 @@
-'use client';
-import { useEffect, useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
 
 export default function CircuitBackground() {
   const [offset, setOffset] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => setOffset(window.scrollY * 0.2);
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div
-      className="fixed inset-0 -z-20 opacity-20"
+      className="fixed inset-0 -z-20 opacity-30"
       style={{
         backgroundImage: "url('/circuit-board.svg')",
-        backgroundRepeat: 'repeat',
-        backgroundSize: '400px 400px',
+        backgroundRepeat: "repeat",
+        backgroundSize: "400px 400px",
         backgroundPosition: `0px ${-offset}px`,
       }}
     />
